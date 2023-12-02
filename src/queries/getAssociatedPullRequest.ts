5e6275ec1945bf418749b9e9f75d75667c71002d
Hidetake Iwata <int128@gmail.com>
2023-12-02 18:36:36 +0900

    Initial commit

---
+++ src/queries/getAssociatedPullRequest.ts
@@ -0,0 +1,30 @@
+import { GitHub } from '@actions/github/lib/utils'
+import { GetAssociatedPullRequestQuery, GetAssociatedPullRequestQueryVariables } from '../generated/graphql'
+
+type Octokit = InstanceType<typeof GitHub>
+
+const query = /* GraphQL */ `
+  query getAssociatedPullRequest($owner: String!, $name: String!, $expression: String!) {
+    rateLimit {
+      cost
+      remaining
+    }
+    repository(owner: $owner, name: $name) {
+      object(expression: $expression) {
+        __typename
+        ... on Commit {
+          associatedPullRequests(first: 1, orderBy: { field: CREATED_AT, direction: ASC }) {
+            nodes {
+              number
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export const getAssociatedPullRequest = async (
+  octokit: Octokit,
+  v: GetAssociatedPullRequestQueryVariables,
+): Promise<GetAssociatedPullRequestQuery> => await octokit.graphql(query, v)
