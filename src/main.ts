33e474be594aa368a5f43239fad8ac865ce129a7
Hidetake Iwata <int128@gmail.com>
2025-11-30 16:26:09 +0900

    Migrate to @octokit/action

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import * as core from '@actions/core'
+import { getOctokit } from './github.js'
 import { run } from './run.js'
 
 const main = async (): Promise<void> => {
-  const outputs = await run({
-    organization: core.getInput('organization', { required: true }),
-    usernames: core.getMultilineInput('usernames', { required: true }),
-    includes: core.getMultilineInput('includes'),
-    limit: Number(core.getInput('limit', { required: true })),
-    addPrefix: core.getInput('add-prefix'),
-    token: core.getInput('token', { required: true }),
-  })
+  const outputs = await run(
+    {
+      organization: core.getInput('organization', { required: true }),
+      usernames: core.getMultilineInput('usernames', { required: true }),
+      includes: core.getMultilineInput('includes'),
+      limit: Number(core.getInput('limit', { required: true })),
+      addPrefix: core.getInput('add-prefix'),
+    },
+    getOctokit(),
+  )
   core.info(`Outputs = ${JSON.stringify(outputs, undefined, 2)}`)
   core.setOutput('teams', outputs.teams.join('\n'))
 }
